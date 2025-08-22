8951480de1862719fc2a664be39148f4a7ca97d9
maestrovt <maestrovt@gmail.com>
2025-08-22 05:57:48 -0400

    CH1_L5

--- main.py
+++ main.py
@@ -2,25 +2,38 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 
 def main():
-    # Check for argument
-    if len(sys.argv) < 2:
-        print("Error: No prompt provided.")
+
+    load_dotenv()
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
 
-    prompt = sys.argv[1]
-    load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    user_prompt = " ".join(args)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=messages,
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
     print("Response:")
     print(response.text)
 
