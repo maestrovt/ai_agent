b72b540a50bd547b3103e6875026d8bed328a24d
maestrovt <maestrovt@gmail.com>
2025-08-24 05:18:36 -0400

    CH1_L6_Verbose

--- main.py
+++ main.py
@@ -17,6 +17,17 @@ def main():
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
 
+    # Check for --verbose
+    verbose = False
+    if "--verbose" in args:
+        verbose = True
+        args.remove("--verbose")
+
+    if not args:
+        print("Error: You must provide a prompt when using --verbose")
+        sys.exit(1)
+
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
@@ -25,14 +36,18 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages)
+    generate_content(client, messages, user_prompt, verbose)
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, user_prompt, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
     print("Response:")
     print(response.text)
